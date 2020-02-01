0c3da78c0c2f1a56037993869f4b98fa20f29653
SeepG <seep.torque@gmail.com>
2020-02-01 19:52:46 +1100

    Testing welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,9 +3,12 @@ package com.twu.biblioteca;
 import static java.lang.System.*;
 
 public class BibliotecaApp {
-
     public static void main(String[] args) {
-        out.println("Hello, world!");
-        out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        BibliotecaApp obj = new BibliotecaApp();
+        out.println(obj.GetWelcomeMessage());
+    }
+    public String GetWelcomeMessage(){
+        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
     }
 }
+
